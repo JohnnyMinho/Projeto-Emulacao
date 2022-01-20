28433ab232ec3c265d08dc41006a7a3876553708
JohnnyMinho <79586002+JohnnyMinho@users.noreply.github.com>
2022-01-20 09:33:23 +0000

    Add files via upload

---
+++ Arduino_fase4_emul/Teste_Java_para_Arduino_Input/Teste_Java_para_Arduino_Input.ino
@@ -0,0 +1,57 @@
+#include "RF24.h"
+#include "nRF24L01.h"
+#include <math.h>
+#include <SPI.h>;
+#include <CRCx.h>;
+
+RF24 radio(16,17); //Configuração dos pinos CE, CSN (Pinos na placa ESP32)
+
+#define TIMEOUT_TIME 50000 // 50 milisegundos
+
+byte Adresses[2][6] = {"00001","00002"}; //Define os endereções usados na comunicação
+byte Packet[32]; // Distribuição da trama, 1 byte para header (2 bits de endereço de origem, 2 bits para endereço de chegade, 3 para tipo e 1 para permitir a verificação da repetição do pacote , 30 bytes de data, 1 byte (8bits dedicados a CRC)
+byte PacketCRC[30];
+byte Ack = 0b000000000;
+byte Aux_Array[30] = " ";
+int array_size = 0;
+String aux_string;
+int posArray = 0; //Só é usada caso seja necessário enviar mais que uma trama
+double times_to_send = 0; //Vai guardar o numero de tramas a enviar
+byte Header_Fim = 0b00101010;
+int counter = 0;
+int counter_receber = 0;
+unsigned long Start_Timer = 0;
+unsigned long Total_Time = 0;
+unsigned long timer = 0;
+bool timeout = false;
+bool I_Started = false;
+bool I_Received_Something = false;
+bool Send_Dif = false; //Como os bits de cada packet são pre-definidos, os bits do packet mudam a cada packet
+byte SequenceBit = 0; //indica o numero da trama
+int Sent_Packets = 0; //Conta o numero de pacotes enviado sendo que no momento em que o n 100 é enviado, é enviado o pacote que sinaliza o final
+int Resent_Packets = 0; //Fica em comentário excepto na verificação do débito
+bool Ack_or_Nack = false; //Indica se foi recebido um Ack (true) ou um Nack(false)
+double debito = 0;
+
+void setup() {
+  Serial.begin(115200); //baudrate de 115200 bit/sec 
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setChannel(100); // 2476 Mhz (RF24 channel 77))83 => 2483 Mhz (RF24 channel 84)
+  radio.disableCRC();
+  radio.openWritingPipe(Adresses[0]);
+  radio.openReadingPipe(1,Adresses[1]);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_1MBPS); //velocidade de transmissão de 1MBPS
+  radio.stopListening(); //Para o listening no modulo RF24 que está a enviar
+}
+
+void loop() {
+  while(true){
+    if(Serial.available() > 0){
+    aux_string = Serial.readStringUntil('\n');
+    }
+    Serial.println(aux_string);
+  }
+  
+}
